1df32253df4ba47679cb04263b117672eea6538b
fraCardoso <franciscoti2022@gmail.com>
2022-05-02 13:26:07 -0300

    Simples autocomplete de endereço google api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => {
+    const placeInputRef = useRef(null);
+    const [place, setPlace] = useState(null);
+  
+    useEffect(() => {        
+      initPlaceAPI();
+    }, []);
+      
+    const initPlaceAPI = () => {
+      let autocomplete = new window.google.maps.places.Autocomplete(placeInputRef.current);
+      new window.google.maps.event.addListener(autocomplete, "place_changed", function () {
+        let place = autocomplete.getPlace();
+        setPlace({
+          address: place.formatted_address,
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng()
+        });
+      });
+    };
+  
+    return (
+      <div className="app-header">
+        <input type="text" ref={placeInputRef} placeholder="Enter for andress..."/>
+        <div className="content">
+          {place && <div style={{ marginTop: 20, lineHeight: '25px' }}>
+            <div style={{ marginBottom: 10 }}><b>Selected Place</b></div>          
+            <div><b>Address:</b> {place.address}</div>
+            <div><b>Lat:</b> {place.lat}</div>
+            <div><b>Lng:</b> {place.lng}</div>
+          </div>}
+        </div>
+      </div>
+    );
+  };
+  
+  export default App;
